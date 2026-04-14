cb4f245dd475a0a24d8750c6e16e669a3521ef83
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2026-04-14 14:42:22 +0500

    task 4

--- lab5/main.py
+++ lab5/main.py
@@ -1,7 +1,21 @@
 from vm import HyperVisor, VirtualMachine
 
+def simulate_apps(*vm_apps: tuple[VirtualMachine, str, int, int, int]) -> None:
+	for vm_app in vm_apps:
+		vm_app[0].show_specs()
 
-def main() -> None:
+	for vm, app_name, cpu, memory, storage in vm_apps:
+		vm.run_app(app_name, cpu, memory, storage)
+
+	for vm, app_name, cpu, memory, storage in vm_apps:
+		stopped_app, utilization = vm.stop_app(app_name, cpu, memory, storage)
+
+		print(f"Resource utilization for {vm.name} - {utilization}")
+
+	for vm_app in vm_apps:
+		vm_app[0].show_specs()
+
+def task3() -> None:
 	hv = HyperVisor(1, "Hypervisor1")
 
 	vm1 = VirtualMachine(1, "VM1", 2, 8, 500)
@@ -17,21 +31,38 @@ def main() -> None:
 
 	hv.list_vms()
 
-	for vm in hv.vms:
-		vm.show_specs()
+	simulate_apps(
+		(vm1, *apps[0]),
+		(vm2, *apps[1]),
+	)
 
-	vm1.run_app(*apps[0])
-	vm2.run_app(*apps[1])
+def task4() -> None:
+	hv = HyperVisor(1, "Hypervisor1")
 
-	for vm in (vm1, vm2):
-		for app in vm.running_applications:
-			stopped_app, utilization = vm.stop_app(*app)
+	vm1 = VirtualMachine(1, "VM1", 8, 16, 1000)
+	vm2 = VirtualMachine(2, "VM2", 16, 32, 1000)
 
-			print(f"Resource utilization for {vm.name} - {utilization}")
+	apps = (
+		("App1", 1, 2, 125),
+		("App2", 1, 6, 250),
+	)
 
-	for vm in (vm1, vm2):
-		vm.show_specs()
+	hv.create_vm(vm1)
+	hv.create_vm(vm2)
 
+	hv.list_vms()
+
+	simulate_apps(
+		(vm1, *apps[0]),
+		(vm1, *apps[1]),
+		(vm2, *apps[0]),
+		(vm2, *apps[1]),
+	)
+
+
+def main() -> None:
+	# task3()
+	task4()
 
 if __name__ == "__main__":
 	main()
